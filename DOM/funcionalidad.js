e8e517e474653e27ab4374de519d8817ed705a42
IrvynCornejo <irvyncornejo@gmail.com>
2019-01-25 08:09:09 -0600

    Ejercicios con DOM

---
+++ DOM/funcionalidad.js
@@ -0,0 +1,15 @@
+var n1;
+var n2;
+var r;
+
+function suma(){
+  n1  = prompt('Primer digito');
+  n2 = = prompt('Segundo digito');
+  r = (parseInt(numero1)) + (parseInt(numero2));
+
+}
+
+
+alert(r);
+suma(n1,n2);
+document.write('El resultado ' + r);
